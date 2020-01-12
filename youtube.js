599e72c8750182c049d794f1ffb980a29bf0e670
hommat <zmateusz1111@gmail.com>
2020-01-12 14:28:32 +0100

    Add youtube_channels.json

--- youtube.js
+++ youtube.js
@@ -1,31 +1,12 @@
 const { google } = require("googleapis");
+const channels = require("./data/youtube_channels.json");
 
 const youtube = google.youtube({
   version: "v3",
   auth: process.env.YOUTUBE_API_KEY
 });
 
-const channelsPlaylistIds = {
-  "The Net Ninja": "UUW5YeuERMmlnqo4oq8vwUpg",
-  "Ben Awad": "UU-8QAzbLcRglXeN_MY9blyw",
-  "Traversy Media": "UU29ju8bIPH5as8OGnQzwJyA",
-  "Basarat Ali": "UUGD_0i6L48hucTiiyhb5QzQ",
-  BookMark: "UUtoBjWAGbdlf9FdeQF3Be9Q",
-  Codegrid: "UU7pVho4O31FyfQsZdXWejEw",
-  DarkCode: "UUD3KVjbb7aq2OiOffuungzw",
-  "Devon Crawford": "UUDrekHmOnkptxq3gUU0IyfA",
-  "freeCodeCamp.org": "UU8butISFwT-Wl7EV0hUK0BQ",
-  "Julio Codes": "UUeHWTyAKBv_WnW8gsEKGECw",
-  "Kurs Reacta": "UU9b88oZkSb3gAWybsVIM6eg",
-  "Online Tutorials": "UUbwXnUipZsLfUckBPsC7Jog",
-  "Online web ustaad": "UU8xTHK97Ng__KZvGcO_K7CA",
-  overment: "UU_MIaHmSkt9JHNZfQ_gUmrg",
-  "The Coding Train": "UUvjgXvBlbQiydffZU7m1_aw"
-};
-
-const playlistsIds = Object.keys(channelsPlaylistIds).map(
-  key => channelsPlaylistIds[key]
-);
+const playlistsIds = Object.keys(channels).map(key => "UU" + channels[key]);
 
 module.exports.getVideos = async videoCount => {
   try {
