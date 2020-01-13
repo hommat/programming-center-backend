cf9ba687d726c9076b5bbafae6b22b21643d391a
hommat <zmateusz1111@gmail.com>
2020-01-13 16:41:27 +0100

    Add reddit route

--- handlers.js
+++ handlers.js
@@ -3,6 +3,7 @@ const { getVideos } = require("./youtube");
 const { badRequest } = require("./errror");
 
 const maxYoutubeVideosCount = 50;
+const maxRedditPosts = 30;
 
 module.exports.getYoutubeVideos = async ({ query: { videoCount } }, res) => {
   if (!videoCount) return badRequest(res, "Video count is required.");
@@ -14,10 +15,9 @@ module.exports.getYoutubeVideos = async ({ query: { videoCount } }, res) => {
   return res.status(200).json(videos);
 };
 
-const githubEndPoint = "https://api.github.com/users/hommat/repos?sort=created";
-
 module.exports.getGithubRepos = async (req, res) => {
-  const repos = await axios.get(githubEndPoint);
+  const endPoint = "https://api.github.com/users/hommat/repos?sort=created";
+  const repos = await axios.get(endPoint);
   const filteredRepos = repos.data.filter(repo => !repo.fork);
   const mappedRepos = filteredRepos.map(
     ({ id, name, html_url, homepage, description }) => ({
@@ -30,4 +30,26 @@ module.exports.getGithubRepos = async (req, res) => {
   );
 
   res.status(200).json(mappedRepos);
+};
+
+module.exports.getRedditPosts = async (
+  { query: { limit = 20, sub = "reactjs", after } },
+  res
+) => {
+  if (limit > maxRedditPosts) {
+    return badRequest(res, `Limit must be <= ${maxRedditPosts}.`);
+  }
+
+  const query = `https://www.reddit.com/r/${sub}/.json?limit=${limit}`;
+  if (after) query += `&after=${after};`;
+
+  const redditRes = await axios.get(query);
+  const posts = redditRes.data.data.children;
+  const mappedPosts = posts.map(post => ({
+    title: post.data.title,
+    score: post.data.ups,
+    name: post.data.name
+  }));
+
+  res.status(200).json(mappedPosts);
 };
--- routes.js
+++ routes.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
-const { getYoutubeVideos, getGithubRepos } = require("./handlers");
+const {
+  getYoutubeVideos,
+  getGithubRepos,
+  getRedditPosts
+} = require("./handlers");
 
 const router = Router();
 
 router.get("/youtube", getYoutubeVideos);
 router.get("/github", getGithubRepos);
+router.get("/reddit", getRedditPosts);
 
 module.exports = router;
